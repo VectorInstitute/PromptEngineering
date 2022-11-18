c3e35c508eb36633de63209fe905080ffb1f1f56
Saeed Najafi <saeed@localadmins-MacBook-Pro-2.local>
2022-11-17 21:04:08 -0500

    Add slurm code to train on sentiment analysis

---
+++ src/reference_implementations/prompt_zoo/train_semeval_sentiment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+PROJECT_DIR=$( dirname -- "$0"; )
+
+# We source to keep the internal env variables defined.
+source ${PROJECT_DIR}/../setup_gpu_worker.sh
+
+
+python trainer.py \
+    --batch_size 8 \
+    --mode train \
+    --task_name semeval_3_class_sentiment \
+    --train_file ${PROJECT_DIR}/../../../resources/datasets/2018-Valence-oc-En-train.txt \
+    --t5_exp_type all_finetune \
+    --gpu True \
+    --model_path /scratch/ssd004/scratch/snajafi/data_temp/torch-prompt/all_finetune \
+    --learning_rate 0.0005 \
+    --max_epochs 10 \
+    --training_steps 10000000 \
+    --steps_per_checkpoint 20 \
+    --source_max_length 128 \
+    --decoder_max_length 16
