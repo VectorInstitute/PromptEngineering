dee01eae6fbaec6f806721dd2772f7ecef5c0f4c
snajafi <snajafi@v.cluster.local>
2022-12-08 16:00:51 -0500

    Add the scripts for the generative examples.

---
+++ train_scripts/run_semeval_sentiment_experiments_generative.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+rates=(0.005)
+
+exps=(all_finetune input_finetune output_finetune soft_prompt_finetune)
+
+for i in ${!rates[@]};
+do
+	rate=${rates[$i]}
+    for j in ${!exps[@]};
+    do
+        exp=${exps[$j]}
+        sbatch src/reference_implementations/run_singlenode_prompt.slrm \
+            src/reference_implementations/prompt_zoo/train_semeval_sentiment.sh \
+            ./torch-prompt-tuning-exps-logs \
+            ${exp} \
+            ${rate} \
+            False
+    done
+done
