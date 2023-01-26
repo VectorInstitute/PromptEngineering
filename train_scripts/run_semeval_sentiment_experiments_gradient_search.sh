4583d55d36e74a54494b92a3dd03ec448e7c3ac7
Saeed Najafi <snajafi@ualberta.ca>
2023-01-26 15:28:19 -0500

    Going to test the experiments.

---
+++ train_scripts/run_semeval_sentiment_experiments_gradient_search.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+rates=(0.5)
+exps=(gradient_search)
+
+for i in ${!rates[@]};
+do
+	rate=${rates[$i]}
+    for j in ${!exps[@]};
+    do
+        exp=${exps[$j]}
+        sbatch src/reference_implementations/run_singlenode_prompt.slrm \
+            src/reference_implementations/prompt_zoo/gradient_search_semeval_sentiment.sh \
+            ./torch-prompt-tuning-exps-logs \
+            ${exp} \
+            ${rate} \
+            True \
+            5
+
+    done
+done
