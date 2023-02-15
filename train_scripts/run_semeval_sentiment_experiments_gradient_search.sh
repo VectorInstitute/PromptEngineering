325d9613272394031e1b7f027fa2f9a54d9b3d82
snajafi <snajafi@v.cluster.local>
2023-02-15 18:19:58 -0500

    re-organize and add large-scale runs for gradient-search

--- train_scripts/run_semeval_sentiment_experiments_gradient_search.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sbatch  src/reference_implementations/run_singlenode_prompt.slrm \
-        src/reference_implementations/prompt_zoo/gradient_search_semeval_sentiment.sh \
-        ./torch-prompt-tuning-exps-logs \
-        gradient_search \
