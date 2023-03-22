02654650b3dbb1c871427ed51706b13dfa6d7c84
David Emerson <43939939+emersodb@users.noreply.github.com>
2023-03-22 10:06:42 -0400

    Bug fixes

--- src/reference_implementations/hugging_face_basics/training_script/finetuning_roberta.sh
+++ src/reference_implementations/hugging_face_basics/training_script/finetuning_roberta.sh
@@ -3,6 +3,6 @@
 PROJECT_DIR=$( dirname -- "$0"; )
 
 # We source to keep the internal env variables defined.
-source ${PROJECT_DIR}/../../../setup_gpu_worker.sh
+source ${PROJECT_DIR}/../../setup_gpu_worker.sh
 
-python -m src.reference_implementations.hugging_face_basics.roberta_fine_tune_script
+python -m src.reference_implementations.hugging_face_basics.hf_fine_tuning_examples.roberta_fine_tune_script
