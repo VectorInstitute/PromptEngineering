cb42c1a2418305b1ad2973669c6765514f25204b
David Emerson <43939939+emersodb@users.noreply.github.com>
2023-03-30 13:22:12 -0400

    Starting a fix on the hf fine tune notebook

--- src/reference_implementations/hugging_face_basics/hf_fine_tuning_examples/custom_dataloaders.py
+++ src/reference_implementations/hugging_face_basics/hf_fine_tuning_examples/custom_dataloaders.py
@@ -32,19 +32,18 @@ def construct_dataloaders(
     )
 
     train_dataset = tokenized_dataset_dict["train"]
+    # Ensure that the dataloader yields PyTorch tensors, not lists of lists.
+    train_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "label"])
 
     if "validation" in tokenized_dataset_dict.keys():
         val_dataset = tokenized_dataset_dict["validation"]
+        val_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "label"])
     else:
         # Some datasets (e.g., AG news) just has train and test sets (no validation set)
         # split the original training dataset into a training and validation set.
         train_dataset, val_dataset = make_train_val_datasets(train_dataset, train_split_ratio)
 
-    # Ensure that the dataloader yields PyTorch tensors, not lists of lists.
-    train_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "label"])
-    val_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "label"])
-
-    # Create the AG news test set.
+    # Create the test set.
     test_dataset = tokenized_dataset_dict["test"]
     test_dataset.set_format(type="torch", columns=["input_ids", "attention_mask", "label"])
 
